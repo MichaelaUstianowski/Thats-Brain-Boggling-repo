1fc95b2e08dae1bbed4e88b7fc64f82e92293114
MichaelaUstianowski <44266495+MichaelaUstianowski@users.noreply.github.com>
2018-12-07 16:20:06 +0000

    Implementation of some JQuery
    Implemented hover buttons and hover popovers. Deleted glossary due to popovers working.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,79 @@
 $(document).ready(function(){
-  $('[data-toggle="popover"]').popover(); 
-   // jQuery methods go here...
+  $("#frontal-lobe-image").hide();
+  $("#temporal-lobe-image").hide();
+  $("#parietal-lobe-image").hide();
+  $("#occipital-lobe-image").hide();
+
+  $("#frontal-lobe-button").hover(function(){
+    $("#frontal-lobe-image").toggle()
+    $("#four-lobes").toggle();
+  });
+
+  $("#parietal-lobe-button").hover(function(){
+      $("#parietal-lobe-image").toggle()
+      $("#four-lobes").toggle();
+});
+
+$("#temporal-lobe-button").hover(function(){
+    $("#temporal-lobe-image").toggle()
+    $("#four-lobes").toggle();
+});
+
+$("#occipital-lobe-button").hover(function(){
+    $("#occipital-lobe-image").toggle()
+    $("#four-lobes").toggle();
+});
+
+$(function () {
+  $('#neuron').popover()
+});
+
+$(function () {
+  $('#cerebral-cortex').popover()
+});
+
+$(function () {
+  $('#corpus-collosum').popover()
+});
+
+$(function () {
+  $('#epilepsy').popover()
+});
+
+$(function () {
+  $('#cognitive-planning').popover()
+});
+
+$(function () {
+  $('#lesions').popover()
+});
+
+$(function () {
+  $('#Stimuli').popover()
+});
+
+$(function () {
+  $('#primary-visual-cortex').popover()
+});
+
+$(function () {
+  $('#V2').popover()
+});
+
+$(function () {
+  $('#PET').popover()
+});
+
+$(function () {
+  $('#V4').popover()
+});
+
+$(function () {
+  $('#V5').popover()
+});
+
+$(function () {
+  $('#limbic-system').popover()
+});
 
 });
